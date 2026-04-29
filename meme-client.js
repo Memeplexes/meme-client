e3d0faa6e91a4552da4f0bbeb649ea72562ea84e
Marak <marak.squires@gmail.com>
2026-04-29 13:59:11 -0400

    Adds `CreatorCard` web component Improved UX

--- meme-client.js
+++ meme-client.js
@@ -1,4 +1,5 @@
 import "./lib/web-components/SearchBarTags.js";
+import "./lib/web-components/CreatorCard.js";
 import "./lib/web-components/MemeCard.js";
 import "./lib/web-components/FloatingOctocat.js";
 import { MemeClient } from "./MemeClient.js";
