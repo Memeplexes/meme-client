976ddd8eae1e046777feff42aa30c2383920d4e5
Marak <marak.squires@gmail.com>
2026-05-03 19:39:53 -0400

    Login flow now immediately shows user session After auth callback front-end will reload ( for now )

--- meme-client.js
+++ meme-client.js
@@ -18,6 +18,7 @@ if (token) {
   localStorage.setItem("access_token", token);
   access_token = token;
   console.log("Logged in with token:", token);
+
   //alert("Login successful! You can now close this window.");
   // hard-reload the page to trigger layout.js?
   // without this reload, we end up getting in a "guest" state
@@ -25,6 +26,10 @@ if (token) {
   // goto("/dashboard"); // Remark: does not properly load account session
 }
 
+// check to see if the incoming url started with /auth/callback, if so we wish to reload the root page to trigger the new authenticated state in the client
+if (window.location.pathname.startsWith("/auth/callback")) {
+  window.location.href = "/";
+}
 
 const memeClient = new MemeClient({
   feed: document.querySelector("#feed"),
